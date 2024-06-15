ee86d89b2c57622178384e5a15335f375d30252d
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-06-15 13:53:16 -0300

    feat: create vendas, recebido and processando in Resumo

--- src/pages/Resumo.tsx
+++ src/pages/Resumo.tsx
@@ -1,11 +1,55 @@
-import React from "react";
 import { useData } from "../context/DataContext";
 
 const Resumo = () => {
   const { data } = useData();
 
-  console.log(data);
-  return <div>Resumo</div>;
+  return (
+    <section>
+      {data === null ? null : (
+        <>
+          <div className="resumo flex mb">
+            <div className="box">
+              <h2>Vendas</h2>
+              <span>
+                {data
+                  .filter((item) => item.status !== "falha")
+                  .reduce((acc, item) => acc + item.preco, 0)
+                  .toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+              </span>
+            </div>
+            <div className="box">
+              <h2>Recebido</h2>
+              <span>
+                {data
+                  .filter((item) => item.status === "pago")
+                  .reduce((acc, item) => acc + item.preco, 0)
+                  .toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+              </span>
+            </div>
+            <div className="box">
+              <h2>Processando</h2>
+              <span>
+                {data
+                  .filter((item) => item.status === "processando")
+                  .reduce((acc, item) => acc + item.preco, 0)
+                  .toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+              </span>
+            </div>
+          </div>
+          <div className="box">Gráficos</div>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default Resumo;
