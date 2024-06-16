95db21516f666040a56391e6cfe298fd901e6218
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-06-16 12:10:37 -0300

    feat: create page venda

--- src/components/VendaItem.tsx
+++ src/components/VendaItem.tsx
@@ -1,11 +1,12 @@
+import { NavLink } from "react-router-dom";
 import { IVendas } from "../context/DataContext";
 
 const VendaItem = ({ venda }: { venda: IVendas }) => {
   return (
     <div className="venda box">
-      <a href="" style={{ fontFamily: "monospace" }}>
+      <NavLink to={`/vendas/${venda.id}`} style={{ fontFamily: "monospace" }}>
         {venda.id}
-      </a>
+      </NavLink>
       <div>{venda.nome}</div>
       <div>
         {venda.preco.toLocaleString("pt-br", {
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,24 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
-interface FetchState<T> {
-  data: T | null;
-  loading: boolean;
-  error: string | unknown;
-}
-
-const useFetch = <T,>(
-  url: RequestInfo | URL,
-  options?: RequestInit
-): FetchState<T> => {
+function useFetch<T>(url: RequestInfo | URL, options?: RequestInit) {
   const [data, setData] = React.useState<T | null>(null);
   const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState<string | unknown>(null);
+  const [error, setError] = React.useState<string | null>(null);
 
-  const optionsRef = useRef(options);
+  const optionsRef = React.useRef(options);
   optionsRef.current = options;
 
-  useEffect(() => {
-    const { signal } = new AbortController();
+  React.useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
 
     const fetchData = async () => {
       setLoading(true);
@@ -33,12 +25,18 @@ const useFetch = <T,>(
         if (!signal.aborted) setData(json);
       } catch (error) {
         if (!signal.aborted && error instanceof Error) setError(error.message);
+      } finally {
+        if (!signal.aborted) setLoading(false);
       }
     };
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return { data, loading, error };
-};
+}
 
 export default useFetch;
