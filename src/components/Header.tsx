7afb6f0013a3f9f63d3f008f52aff7ef1937c799
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-06-16 11:34:00 -0300

    feat: create a routes for resumo and vendas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../context/DataContext";
 import DateRange from "./DateRange";
 import Meses from "./Meses";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const [title, setTitle] = useState("Resumo");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setTitle("Resumo");
+      document.title = "Fintech | Resumo";
+    } else {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    }
+  }, [location.pathname]);
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="mb daterange">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Meses />
     </header>
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import sidenav from "../../config/sidenav.json";
+import FintechSVG from "../../assets/FintechSVG";
+import { NavLink } from "react-router-dom";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech Logo" />
+      <ul>
+        {sidenav.map((item) => (
+          <li>
+            <span>
+              <img src={item.image} alt="" />
+            </span>
+            <NavLink to={item.path}>{item.name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
